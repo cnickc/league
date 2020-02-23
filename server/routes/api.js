f94126a2a6d80c02f7f6f8ad3106b18a16304ae4
cam.christou <cam.christou@e-bydesign.ca>
2020-02-23 18:40:12 -0500

    Documenting thoughts for later
    Just some implementation comments that I don't want to forget.

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
 
-const secretKey = 'secret key' //consider putting this into a config file
+const secretKey = 'secret key' //consider putting this into a config file. Could also make this dynamic and change per user every time that user logs out
 const router = express.Router()
 
 router.get('/', async (req, res) => {
@@ -39,7 +39,7 @@ function verifyToken(req, res, next) {
     //check that token exists.  If not, send 403. Token signature and payload should exist in separate cookies
 
     //fake out token for now. Copy output from /api/login endpoint and paste here to test
-    req.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Im5hbWUiOiJ0ZXN0IHVzZXIiLCJyb2xlIjoiY29tbWlzc2lvbmVyIn0sImlhdCI6MTU4MjQ5MjE3OSwiZXhwIjoxNTgyNDkyNzc5fQ.Kcr4hOqnVf4o_c-0yINmM08MAob8fd60oq8tsZSwvwk'
+    req.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Im5hbWUiOiJ0ZXN0IHVzZXIiLCJyb2xlIjoiY29tbWlzc2lvbmVyIn0sImlhdCI6MTU4MjUwMDUyOCwiZXhwIjoxNTgyNTAxMTI4fQ.FOSFImBP4PISiALyOotaO50ApR7AJIO5FyQmMEZxQQw'
     next()
 }
 
